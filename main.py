36a9f9cc0da72f04d0cf18376df071895b74e8c4
Sameer Raza <draza8757@gmail.com>
2026-01-02 12:42:50 +0530

    FastApi

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI  #Imported FastAPI class from fastapi
+import json
+
+
+app = FastAPI()   #created an object
+
+def load_data():
+    with open('Patients.json' , 'r') as f:
+        data = json.load(f)
+
+        return data
+
+
+
+@app.get("/")     #get here is used to fetch somthing from the server , post is used to post somthing in the server
+
+def greet():
+    return {'messege':"Patient Management System API "}
+
+@app.get('/about')
+
+def about():
+    return {'messege' :'A Fully Functional API Management System To Manage Patient Records  ' }
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
